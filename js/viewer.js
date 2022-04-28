c3fedecc5598537008d8ae9e80bc82ce4bb518e8
Mike Jones <michaeljones401@gmail.com>
2022-04-28 17:33:54 +0100

    zoom to article

--- js/viewer.js
+++ js/viewer.js
@@ -1,4 +1,5 @@
 mainTileSource = '';
+articles = [];
 
 viewer = {
 
@@ -33,12 +34,12 @@ viewer = {
     
     // osd.destroy()
     json = this.manifestRequest(manifest)
-    console.log(json)
-    console.log("Name click " + name)
+    // console.log(json)
+    // console.log("Name click " + name)
     _this = this;
 
     var urlParts = json.target.id.split("/");
-    console.log(urlParts)
+    // console.log(urlParts)
     
     pidHash = urlParts[6]
     pidHashParts = pidHash.split("#")
@@ -58,18 +59,86 @@ viewer = {
     var boundaries = 'https://newspapers.library.wales/json/viewarticledata/llgc-id%3A' + pid;
 
     this.loadAnnotation(osd, json, boundaries, name, pid);
+    this.panToArticle(manifest);
+  },
 
+  panToArticle: function(manifest) {
+    console.log(manifest)
+    var manifestUrlparts = manifest.split('/');
+    var articleParts = manifestUrlparts[8].split('-');
+    var pid = articleParts[2];
+    var art = parseInt(articleParts[1].replace('modsarticle',''))
+    // art = art + 1;
+    art = 'ART'+ art
+    
+    var url = 'https://newspapers.library.wales/json/viewarticledata/llgc-id%3A' + pid;
+
+    json = this.manifestRequest(url)
+
+    var blocks = [];
+
+    $.each(json, function(k, v){
+      var id = v['id'];
+
+      if(id == art) {
+        console.log(v)
+        blocks.push(v['textBlocks'])
+      }
+    });
+
+    this.snapToArticle(blocks)
+  },
+
+  snapToArticle: function (blocks) {
+    blocks = blocks[0]
+
+    var chunks = [];
+    $.each(blocks, function(k,v){
+      chunks.push({
+        'x': v['x'],
+        'y': v['y'],
+        'h': v['h'],
+        'w': v['w']
+      });
+    });
+    // console.log(chunks)
+    var startX = 99;
+    var startY = 99;
+    var finalX = 0;
+    var finalY = 0;
+
+    for (i = 0; i < chunks.length; i++) {
+        if (chunks[i].x < startX) {
+            startX = chunks[i].x;
+        }
+        if (chunks[i].y < startY) {
+            startY = chunks[i].y;
+        }
+
+        if (chunks[i].w > finalX) {
+            finalX = chunks[i].w;
+        }
+        if (chunks[i].h > finalY) {
+            finalY = chunks[i].h;
+        }
+    }
+
+    console.log('LOOOK')
+    console.log(startX + ' ' + startY + ' ' + finalX + ' ' +  finalY)
+    // Add some padding the the article area
+    region = new OpenSeadragon.Rect(startX - 0.01, startY - 0.01, finalX + 0.02, finalY + 0.02);
+    osd.viewport.fitBounds(region);
   },
 
   loadAnnotation: function(osd, json, boundaries, name, pid){
-    console.log('load annot')
-    var target = json.target.id;
-    var coords = target.substring(target.indexOf("xywh=") + 5);
-    var elements = coords.split(",");
-    var bounds = osd.viewport.imageToViewportRectangle(parseInt(elements[0]), parseInt(elements[1]), parseInt(elements[2]), parseInt(elements[3]));
+    // console.log('load annot')
+    // var target = json.target.id;
+    // var coords = target.substring(target.indexOf("xywh=") + 5);
+    // var elements = coords.split(",");
+    // var bounds = osd.viewport.imageToViewportRectangle(parseInt(elements[0]), parseInt(elements[1]), parseInt(elements[2]), parseInt(elements[3]));
 
-    console.log(bounds)
-    osd.viewport.fitBounds(bounds, true);
+    // console.log(bounds)
+    // osd.viewport.fitBounds(bounds, true);
 
     this.osdHighlights(boundaries, name, pid)
   },
@@ -91,6 +160,12 @@ viewer = {
 
 
   osdHighlights: function(boundaries, words, pid){
+
+    // clear any existing overlays
+    $('.highlight-segment').each(function(){
+      var id = this.id
+      osd.removeOverlay(id)
+    });
 
     var highlightedwords = 'https://newspapers.library.wales/json/viewarticlewords/llgc-id:' + pid;
 
@@ -127,7 +202,7 @@ viewer = {
               // Add the overview to OSD using the above region coordinates and dimensions.
               osd.addOverlay(element[0], new OpenSeadragon.Rect(region.x, region.y, region.w, region.h));
           });
-          // articles[article.id] = article;
+          articles[article.id] = article;
 
           // Load the word highlighting for each article in sequence.
           if (words.length > 0) {
@@ -135,6 +210,7 @@ viewer = {
                   $.each(data, function(index, region) {
                       var element = document.createElement('div');
                       element.className = 'highlight-segment';
+                      element.id = 'highlight-segment-'+index;
                       osd.addOverlay(element, new OpenSeadragon.Rect(region.x, region.y, region.w, region.h));
                   });
               });
